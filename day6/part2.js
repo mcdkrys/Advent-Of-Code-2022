f1a30439ea6a87a7adfb7eecfc6de0114ab716c8
mcdkrys <mcd.krys@gmail.com>
2022-12-06 11:10:57 +0300

    Day 6

---
+++ day6/part2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const file = fs.readFileSync("./day6/input.txt", "utf-8")
+
+var count = 0;
+
+const chars = () => file.slice(count,count+14)
+
+while ((/([a-zA-Z]).*?\1/).test(chars())) {
+    count++
+}
+
+console.log("Answer: " + (count + 14));
+
